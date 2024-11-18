41c6f6d1714993ff69433699a986bac48f670b49
phocuz <victorishola580@gmail.com>
2024-11-18 02:45:31 +0100

    added new feature and update the code

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,22 +1,48 @@
 import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
+import FormRow from "../../ui/FormRow"
 import Input from '../../ui/Input';
+import Spinner from "../../ui/Spinner"
+import useSetting from './useSetting';
+import useUpdateSetting from './useUpdatesetting';
 
 function UpdateSettingsForm() {
+
+  const {isLoading,settings:{
+    minimumBookingLength,
+    maxBookingLength,
+    maxGuestPeople,
+    breakfastPrice,
+}={}} = useSetting()
+
+const {isUpdating,updateSetting} = useUpdateSetting()
+if(isLoading) return <Spinner />
+
+
+function handleUpdate(e, field){
+  const {value} = e.target;
+  
+  if(!value) return;
+  updateSetting({[field]: value})
+}
   return (
     <Form>
+
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' defaultValue={minimumBookingLength}  disabled={isUpdating} onBlur={(e)=>handleUpdate(e, "minimumBookingLength")}/>
       </FormRow>
+
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights'defaultValue={maxBookingLength}  disabled={isUpdating} onBlur={(e)=>handleUpdate(e, "maxBookingLength")} />
       </FormRow>
+
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' id='max-guests' defaultValue={maxGuestPeople}  disabled={isUpdating} onBlur={(e)=>handleUpdate(e, "maxGuestPeople")}/>
       </FormRow>
+
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' id='breakfast-price' defaultValue={breakfastPrice}  disabled={isUpdating} onBlur={(e)=>handleUpdate(e, "breakfastPrice")}/>
       </FormRow>
+
     </Form>
   );
 }
